5750dbf6740ef4948491407724d6a13dffebc6ab
Bruno Oliveira <dosantosbru@icloud.com>
2024-07-06 14:24:03 -0300

    atualizações

---
+++ decres_cresc.py
@@ -0,0 +1,3 @@
+valor = (input("digite um valor da sequencia: "))
+valores_ordenados = sorted(valor)
+            print(f"Os valores ordenados são: {valores_ordenados}")
---
+++ potenciacao.py
@@ -0,0 +1,10 @@
+import os
+import math
+
+i = 0
+
+while i <= 10:
+    print(2 ** i)
+    i = i + 1
+
+
