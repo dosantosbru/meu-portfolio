b7a13f7b6f5fb461255f97bd55213b17d05bfec6
Bruno Oliveira <dosantosbru@icloud.com>
2024-07-21 21:26:15 -0300

    'testes automatizados'

--- Introducao_ciencia_computacao_python_USP/tests/test_fatorial.py
+++ Introducao_ciencia_computacao_python_USP/tests/test_fatorial.py
@@ -7,5 +7,15 @@ def fatorial(x):
     return math.factorial(x)
 
 
+"""
+def fatorial(x):
+    i = fat = 1
+    while i <= x:
+        fat *= i
+        i += 1
+    return fat
+"""
+
+
 def test_fatorial5():
     assert fatorial(5) == 120
