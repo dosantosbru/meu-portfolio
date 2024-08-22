f668bb5e3cf4c695e589ffbef7bc1abfea6782c6
Bruno Oliveira <dosantosbru@icloud.com>
2024-08-22 20:19:20 -0300

    matrizes

---
+++ Introducao_ciencia_computacao_python_USP/matrizes.py
@@ -0,0 +1,17 @@
+def matriz(numero_linhas, numero_colunas, valor):
+
+    matriz = []
+    for i in range(numero_linhas):
+        linha = []
+        for j in range(numero_colunas):
+            linha.append(valor)
+
+        matriz.append(linha)
+
+    return matriz
+
+
+minha_matriz = matriz(3, 3, 0)
+
+for linha in minha_matriz:
+    print(linha)
